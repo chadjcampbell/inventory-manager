169861e744bd170dfd1082928dca5ddd11205b25
chadjcampbell <chadjcampbell@gmail.com>
2023-06-15 18:11:25 -0400

    set up User mongoose model

--- server/server.js
+++ server/server.js
@@ -5,8 +5,17 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+// middlwares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
 // connect to DB and start server
 mongoose
