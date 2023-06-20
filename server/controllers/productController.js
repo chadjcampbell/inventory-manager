aeb1730d32389e73ce773a8994569258181f3d82
chadjcampbell <chadjcampbell@gmail.com>
2023-06-20 11:22:48 -0400

    build out create product controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,8 +1,64 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const Product = require("../models/productModel");
 
-const createProduct = asyncHandler(async (req, res) => {
-  res.send("Product created");
-});
+const createProduct = [
+  // validate request
+  body("name")
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage("Please enter product name"),
+  body("sku")
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage("Please enter product sku"),
+  body("category")
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage("Please enter product category"),
+  body("quantity")
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage("Please enter product quantity"),
+  body("price")
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage("Please enter product price"),
+  body("description")
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage("Please enter product description"),
+  asyncHandler(async (req, res) => {
+    const { name, sku, category, quantity, price, description } = req.body;
+    // extract the validation errors from a request
+    const errors = validationResult(req);
+    // there are errors
+    if (!errors.isEmpty()) {
+      res.status(400);
+      throw new Error(errors.array()[0].msg);
+    }
+
+    // manage image upload
+
+    // creat product
+    const product = await Product.create({
+      user: req.user.id,
+      name,
+      sku,
+      category,
+      quantity,
+      price,
+      description,
+    });
+    res.status(201).json(product);
+  }),
+];
 
 module.exports = {
   createProduct,
--- server/models/productModel.js
+++ server/models/productModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema(
   {
-    userId: {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
