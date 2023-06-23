82c7576fd78f57e076ac50a061f8fb5cbce2105d
chadjcampbell <chadjcampbell@gmail.com>
2023-06-23 13:56:38 -0400

    set up contact route/controller

---
+++ server/controllers/contactController.js
@@ -0,0 +1,7 @@
+const asyncHandler = require("express-async-handler");
+
+const contactUs = asyncHandler(async (req, res) => {
+  res.send("contact us");
+});
+
+module.exports = { contactUs };
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
 const errorHandler = require("./middleware/errorMiddleware");
 const cookieParser = require("cookie-parser");
 const path = require("path");
@@ -22,6 +23,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // routes middleware
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
 // routes
 app.get("/", (req, res) => {
