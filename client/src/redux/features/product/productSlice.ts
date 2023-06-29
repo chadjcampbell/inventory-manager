d54eff006882d7028be0980755f3f82b3f974de9
chadjcampbell <chadjcampbell@gmail.com>
2023-06-29 10:45:27 -0400

    start redux store for products

---
+++ client/src/redux/features/product/productSlice.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  product: null,
+  products: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    CALC_STORE_VALUE(state, action) {
+      console.log("store vlaue");
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { CALC_STORE_VALUE } = productSlice.actions;
+
+export default productSlice.reducer;
