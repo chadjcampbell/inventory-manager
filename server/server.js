d02a8a963412b84f4e77af9fcc374f214d490840
chadjcampbell <chadjcampbell@gmail.com>
2023-07-11 16:03:56 -0400

    add frontend url to CORS

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", " https://instock-three.vercel.app"],
     credentials: true,
   })
 );
