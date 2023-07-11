0a23f7eec77acfbd46811d4c6eda74db5b7f0876
chadjcampbell <chadjcampbell@gmail.com>
2023-07-11 16:33:19 -0400

    remove space from CORS string

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", " https://instock-three.vercel.app"],
+    origin: ["http://localhost:5173", "https://instock-three.vercel.app"],
     credentials: true,
   })
 );
